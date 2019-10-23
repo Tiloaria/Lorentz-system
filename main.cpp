0bfd7c20312897e6feba47b3bf0486c7d4b79bbb
Tiloaria <savonula@yandex.ru>
2019-10-23 11:12:57 +0300

    Implicit Euler

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 struct Point {
     long double x, y, z, t;
@@ -10,21 +11,48 @@ struct Point {
     }
 };
 
-const long double delta, b, r, delta_t, x0, y0, z0, t0, max_t;
+const long double delta = 0,
+b = 0,
+r = 0,
+delta_t = 0,
+x0 = 0,
+y0 = 0,
+z0 = 0,
+t0 = 0,
+max_t = 0;
 
-Point explicit_method_eiler () {
+std::vector<Point> explicit_method_euler () {
     Point a(x0, y0, z0, t0);
+    std::vector<Point> ps;
+    ps.push_back(a);
     while (a.t < max_t) {
         long double new_x = a.x + delta_t * delta * (a.y - a.x),
         new_y = a.y + delta_t * (-a.x * a.z + r * a.x + a.y),
         new_z = a.z + delta_t * (-a.x * a.y - b * a.z),
-        new_t = t + delta_t;
+        new_t = a.t + delta_t;
         a = Point(new_x, new_y, new_z, new_t);
+        ps.push_back(a);
     }
-    return a;
+    return ps;
 }
 
-Point 
+std::vector<Point> implicit_method_euler () {
+    Point a(x0, y0, z0, t0);
+    std::vector<Point> ps;
+    ps.push_back(a);
+    while (a.t < max_t) {
+        long double new_x = a.x + delta_t * delta * (a.y - a.x),
+                new_y = a.y + delta_t * (-a.x * a.z + r * a.x + a.y),
+                new_z = a.z + delta_t * (-a.x * a.y - b * a.z),
+                new_t = a.t + delta_t;
+        long double n_x = a.x + delta_t / 2 * (delta * (a.y - a.x) + delta * (new_y - new_x)),
+        n_y = a.y + delta_t / 2 * ((-a.x * a.z + r * a.x + a.y) + (-new_x * new_z + r * new_x + new_y)),
+        n_z = a.z + delta_t / 2 * ((-a.x * a.y - b * a.z) + (-new_x * new_y - b * new_z));
+        a = Point (n_x, n_y, n_z, new_t);
+        ps.push_back(a);
+    }
+    return ps;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
